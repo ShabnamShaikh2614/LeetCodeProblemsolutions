5210b7a30db49a7ccffcda2b2c9e61d4f14566ba
Shabnam Shaikh <shabnamshaikh.2614@gmail.com>
2026-08-15 13:09:42 +0530

    Add fourSum method for finding quadruplets
    Implement fourSum method to find unique quadruplets that sum to target.

--- Arrays/4sumsArrays.java
+++ Arrays/4sumsArrays.java
@@ -1,1 +1,100 @@
+/*
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
 
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+Example 2:
+
+Input: nums = [2,2,2,2,2], target = 8
+Output: [[2,2,2,2]]
+ */
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        int n = nums.length;
+
+        // Sort the array
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 3; i++) {
+
+            // Skip duplicate values for i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            // First optimization
+            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+
+            // Second optimization
+            if ((long) nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                // Skip duplicate values for j
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int p = j + 1;
+                int q = n - 1;
+
+                while (p < q) {
+
+                    long sum = (long) nums[i]
+                             + nums[j]
+                             + nums[p]
+                             + nums[q];
+
+                    if (sum < target) {
+                        p++;
+                    } 
+                    else if (sum > target) {
+                        q--;
+                    } 
+                    else {
+                        // Found a valid quadruplet
+                        ans.add(Arrays.asList(
+                            nums[i],
+                            nums[j],
+                            nums[p],
+                            nums[q]
+                        ));
+
+                        p++;
+                        q--;
+
+                        // Skip duplicates for p
+                        while (p < q && nums[p] == nums[p - 1]) {
+                            p++;
+                        }
+
+                        // Skip duplicates for q
+                        while (p < q && nums[q] == nums[q + 1]) {
+                            q--;
+                        }
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
